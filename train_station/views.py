180dc152a186a5313a190fc149d793c11a9d3595
dmitriy kim <d.villarionovich@gmail.com>
2024-06-25 22:24:34 +0200

    created base serializer view for all serializers

--- train_station/views.py
+++ train_station/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from pip._vendor.rich.status import Status
 from rest_framework import viewsets
+from rest_framework.routers import Route
 
-from train_station.models import TrainType
-from train_station.serializers import TrainTypeSerializer
+from train_station.models import TrainType, Train, Ticket, Order, Station, Journey
+from train_station.serializers import TrainTypeSerializer, TrainSerializer, TicketSerializer, OrderSerializer, \
+    StationSerializer, RouteSerializer, JourneySerializer
 
 
 class TrainTypeModelView(viewsets.ModelViewSet):
     queryset = TrainType.objects.all()
     serializer_class = TrainTypeSerializer
+
+
+class TrainModelView(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class TicketModelView(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderModelView(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class StationModelView(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteModelView(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class JourneyModelView(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
