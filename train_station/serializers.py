3af67b549c152f779cee308334f4e44197b062ad
dmitriy kim <d.villarionovich@gmail.com>
2024-06-25 22:24:05 +0200

    created base serializer for all models

--- train_station/serializers.py
+++ train_station/serializers.py
@@ -1,9 +1,44 @@
 from rest_framework import serializers
 
-from train_station.models import TrainType
+from train_station.models import TrainType, Train, Ticket, Order, Station, Route, Journey
 
 
 class TrainTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrainType
+        fields = '__all__'
+
+
+class TrainSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Train
+        fields = '__all__'
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = '__all__'
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = '__all__'
+
+
+class JourneySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Journey
         fields = '__all__'
