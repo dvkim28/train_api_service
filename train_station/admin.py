fbe48b62ce95f56a3943959d16aab5fde7062a24
dmitriy kim <d.villarionovich@gmail.com>
2024-06-25 22:23:49 +0200

    added to admin panels all existing models

--- train_station/admin.py
+++ train_station/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from train_station.models import Station, Train, TrainType, Route, Journey, Order
+
+admin.site.register(Station)
+admin.site.register(Train)
+admin.site.register(TrainType)
+admin.site.register(Route)
+admin.site.register(Journey)
+admin.site.register(Order)
