ba7f546f9f41f102f97909ef5120da25d588dc41
dmitriy kim <d.villarionovich@gmail.com>
2024-06-28 22:21:14 +0200

    bd updated

--- config/settings.py
+++ config/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "default_db_name"),
-        "USER": os.getenv("DB_USER", "default_db_user"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "default_db_password"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
