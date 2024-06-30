6731fae77b9a55d3673cba4f673257f10b925de7
dmitriy kim <d.villarionovich@gmail.com>
2024-07-01 00:43:09 +0200

    media support added

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,5 +30,4 @@ urlpatterns = [
         "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-    ),
-]
+    )] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
