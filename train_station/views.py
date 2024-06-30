19cb9052e5c1da358ada74da0ff348ef323ef11d
dmitriy kim <d.villarionovich@gmail.com>
2024-07-01 01:37:57 +0200

    select related added

--- train_station/views.py
+++ train_station/views.py
@@ -1,44 +1,24 @@
 from django.db.models import Count, F
-from drf_spectacular.utils import (
-    extend_schema,
-    OpenApiParameter,
-    OpenApiExample
-)
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from train_station.models import (
-    Journey,
-    Order,
-    Route,
-    Station,
-    Ticket,
-    Train,
-    TrainType,
-)
+from train_station.models import Journey, Order, Route, Station, Ticket, Train, TrainType
 from train_station.serializers import (
-    JourneyListSerializer,
-    JourneySerializer,
-    OrderListSerializer,
-    OrderSerializer,
-    RouteListSerializer,
-    RouteSerializer,
+    JourneyListSerializer, JourneySerializer,
+    OrderListSerializer, OrderSerializer,
+    RouteListSerializer, RouteSerializer,
     StationSerializer,
     TicketSerializer,
-    TrainListSerializer,
-    TrainRetrieveSerializer,
-    TrainSerializer,
-    TrainTypeSerializer,
+    TrainListSerializer, TrainRetrieveSerializer, TrainSerializer, TrainTypeSerializer
 )
-
 
 class TrainTypeModelView(viewsets.ModelViewSet):
     queryset = TrainType.objects.all()
     serializer_class = TrainTypeSerializer
 
-
 class TrainModelView(viewsets.ModelViewSet):
-    queryset = Train.objects.all()
+    queryset = Train.objects.all().select_related("train_type")
     serializer_class = TrainSerializer
 
     def get_serializer_class(self):
@@ -46,48 +26,31 @@ class TrainModelView(viewsets.ModelViewSet):
             return TrainListSerializer
         elif self.action == "retrieve":
             return TrainRetrieveSerializer
-        else:
-            return TrainSerializer
+        return TrainSerializer
 
-
-class TicketModelView(
-    viewsets.GenericViewSet,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.ListModelMixin,
-):
-    queryset = Ticket.objects.all()
+class TicketModelView(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin):
+    queryset = Ticket.objects.all().select_related("journey", "order")
     serializer_class = TicketSerializer
     permission_classes = (IsAuthenticated,)
 
-    def get_serializer_class(self):
-        if self.action == "create":
-            return TicketSerializer
-        else:
-            return TicketSerializer
-
     def get_queryset(self):
-        return Ticket.objects.filter(order__user=self.request.user)
-
+        return Ticket.objects.filter(order__user=self.request.user).select_related("journey", "order")
 
 class OrderModelView(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().select_related("user")
     serializer_class = OrderSerializer
 
     def get_serializer_class(self):
-        if self.action in ("create"):
-            return OrderSerializer
-        elif self.action == "list":
+        if self.action == "list":
             return OrderListSerializer
+        return OrderSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
 class StationModelView(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
-
 
 class RouteModelView(viewsets.ModelViewSet):
     queryset = Route.objects.all()
@@ -96,22 +59,16 @@ class RouteModelView(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "list":
             return RouteListSerializer
-        elif self.action == "create":
-            return RouteSerializer
         return RouteSerializer
 
     def get_queryset(self):
         queryset = self.queryset
-        source = self.request.query_params.get("source", None)
-        destination = self.request.query_params.get("destination", None)
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
         if source:
-            queryset = queryset.filter(
-                source__name__icontains=source
-            )
+            queryset = queryset.filter(source__name__icontains=source)
         if destination:
-            queryset = queryset.filter(
-                destination__name__icontains=destination
-            )
+            queryset = queryset.filter(destination__name__icontains=destination)
         return queryset
 
     @extend_schema(
@@ -124,8 +81,7 @@ class RouteModelView(viewsets.ModelViewSet):
                 examples=[
                     OpenApiExample(
                         "Example 1",
-                        description='Find route with '
-                                    'destination "Gare do Oriente"',
+                        description='Find route with destination "Gare do Oriente"',
                     )
                 ],
             ),
@@ -137,8 +93,7 @@ class RouteModelView(viewsets.ModelViewSet):
                 examples=[
                     OpenApiExample(
                         "Example 1",
-                        description='Find route with '
-                                    'destination "Gare do Oriente"',
+                        description='Find route with destination "Gare do Oriente"',
                     )
                 ],
             ),
@@ -147,25 +102,22 @@ class RouteModelView(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
-
 class JourneyModelView(viewsets.ModelViewSet):
-    queryset = Journey.objects.all()
+    queryset = Journey.objects.all().select_related("route", "train")
     serializer_class = JourneySerializer
 
     def get_serializer_class(self):
         if self.action == "list":
             return JourneyListSerializer
-        elif self.action == "retrieve":
-            return JourneySerializer
         return JourneySerializer
 
     def get_queryset(self):
-        queryset = self.queryset
-        if self.action in ("list"):
+        queryset = super().get_queryset()
+
+        if self.action == "list":
             queryset = queryset.annotate(
-                holded=Count("tickets"),
-                tickets_available=F("train__places_in_cargo") - F("holded"),
+                booked_seats=Count("tickets"),
+                available_seats=F("train__places_in_cargo") - F("booked_seats"),
             )
-            return queryset
-        else:
-            return queryset
+
+        return queryset
